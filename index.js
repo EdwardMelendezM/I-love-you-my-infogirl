8ab0bae81a0a2491765e5f39c7218a64872c39b3
EdwardMelendezM <192666@unsaac.edu.pe>
2023-06-02 18:48:32 -0500

    adding basic functinality

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const d = document;
-const buttonNo = d.querySelector('.button_no')
-buttonNo.addEventListener('click',e=>{
-  console.log("Esto es un click");
+const buttonNo = d.querySelector('#button_no')
+const buttonYes = d.querySelector('#button_si')
+const nuevoTexto = ' quieroooo';
+var intervalo;
+var indice = 0;
+
+// Button para el NO
+buttonNo.addEventListener('mouseover',e=>{
+  
+  clearInterval(intervalo); // Limpiamos cualquier intervalo anterior
+  indice = 0; // Reiniciamos el índice
+  buttonNo.textContent='Si'
+  intervalo = setInterval(function () {
+    if (indice < nuevoTexto.length) {
+      buttonNo.textContent += nuevoTexto.charAt(indice);
+      indice++;
+    } else {
+      clearInterval(intervalo); // Detenemos el intervalo cuando se complete el texto
+    }
+  }, 100);
 });
+buttonNo.addEventListener('mouseout',e=>{
+  clearInterval(intervalo); // Limpiamos cualquier intervalo activo
+  buttonNo.textContent = 'No';
+});
+
+// Button para el NO
+// buttonYes.addEventListener('mouseover', e => {
+//   buttonYes.textContent = 'Si quierooo'
+// });
+// buttonYes.addEventListener('mouseout', e => {
+//   buttonYes.textContent = 'Si'
+// });
